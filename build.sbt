236f4e9a83c2c90f579b5cfe854618c498b1d648
LudmilaNovikova <Ludmila.Novikova@artezio.com>
2015-12-09 10:51:48 +0300

    working version without sql

--- build.sbt
+++ build.sbt
@@ -4,6 +4,10 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "1.5.2"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.5.2" % "provided"
 
+/*
 libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "1.8.0"
+*/
+
+assemblyOption in assembly := (assemblyOption in assembly).value.copy(includeScala = false)
--- src/main/scala/com/artezio/novikova/ludmila/MainCrimes.scala
+++ src/main/scala/com/artezio/novikova/ludmila/MainCrimes.scala
@@ -3,35 +3,42 @@ package com.artezio.novikova.ludmila
 import java.util.Date
 
 import org.apache.spark.{SparkContext, SparkConf}
-import org.joda.time.DateTime
 
 /**
  * Created by dvlar on 06.12.2015.
  */
 object MainCrimes {
   def main(args: Array[String]) {
-    val file = "/guest/SacramentocrimeJanuary2006.csv"
+    println("Hi, there")
+    val file = "C:\\Users\\lnovikova\\Downloads\\SacramentocrimeJanuary2006.csv"
     val conf = new SparkConf().setAppName("Crimes in Sacramento districts calculation")
     conf.setMaster("local[2]")
     val sc = new SparkContext(conf)
-    val crimes_str = sc.textFile(file, 2).cache()
+    try{
+      val crimes_str = sc.textFile(file, 2).cache().filter(!_.contains("cdatetime"))
+      println("crimes size: " + crimes_str.count())
 
-    case class Crime(
-                      cdatetime: DateTime,
-                      address: String,
-                      district: String,
-                      beat: Int,
-                      grid: String,
-                      crimedescr: String,
-                      ucr_ncic_code: Int,
-                      latitude: BigDecimal,
-                      longitude: BigDecimal)
+      case class Crime(
+                        cdatetime: Date,
+                        address: String,
+                        district: String,
+                        beat: String,
+                        grid: String,
+                        crimedescr: String,
+                        ucr_ncic_code: Int,
+                        latitude: BigDecimal,
+                        longitude: BigDecimal)
 
+      val format = new java.text.SimpleDateFormat("MM/dd/yy")
 
-    val crimes = crimes_str.map(_.split(",")).map(r => Crime(DateTime.parse(r(0)), r(1), r(2), r(3).toInt, r(4),
-      r(5), r(6).toInt, BigDecimal(r(7)), BigDecimal(r(8))))
-    val district_crimes = crimes.groupBy(f => f.district).mapValues(_.sum)
-
-
+      val crimes = crimes_str.map(r => { val elements = r.split(",")
+        Crime(format.parse(elements(0)), elements(1), elements(2), elements(3), elements(4),
+          elements(5), elements(6).toInt, BigDecimal(elements(7)), BigDecimal(elements(8)))})
+      val district_crimes = crimes.groupBy(f => f.district).mapValues(_.size).collect()
+      district_crimes.foreach(println(_))
+     }finally {
+      sc.stop()
+    }
+//    Thread.sleep(10000)
   }
 }
